6ee8a9201ef10aa7dfd1e04e95b49de5b2dd6920
Maciej Szulc <szulcmaciej1@gmail.com>
2021-08-29 20:52:41 +0200

    updates for heroku

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from RevolutBudget.dash_app.app import RevolutBudgetDashApp
 
 revolut_budget_app = RevolutBudgetDashApp()
 server = revolut_budget_app.dash_app.server
-revolut_budget_app.run()
+
+if __name__ == '__main__':
+    revolut_budget_app.run()
